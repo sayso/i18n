271278d11ef30bdf268f2b23073ad8cadb276023
knapo <knapo@knapo.net>
2010-01-14 17:06:28 +0100

    Improve ZuperFast Backend

--- lib/i18n/backend/zuper_fast.rb
+++ lib/i18n/backend/zuper_fast.rb
@@ -9,41 +9,27 @@ module I18n
       protected
 
       #
-      # Converts hash containing both flattened and unflattened keys
-      # to fully winded
+      # Overwrites flatten_hash to unify hash keys first
       #
-      # zuper_hash({:a=>'a', :b=>{:c=>'c', :d=>'d', :f=>{:x=>'x'}}, :"g.h.j" => "j", :"g.h.k" => "k"})
-      # =>
-      # {
-      #  :"b.f.x"=>"x", :"g.h.j"=>"j", :"b.c"=>"c", :g=>{:h=>{:j=>"j", :k=>"k"}}, :"g.h.k"=>"k", :a=>"a",
-      #  :"g.h"=>{:j=>"j", :k=>"k"}, :"b.d"=>"d", :b=>{:d=>"d", :f=>{:x=>"x"}, :c=>"c"}, :"b.f"=>{:x=>"x"}
-      # }
+      # flatten_hash({:a=>"a", :b=>{:c=>"c}, :"g.h.j" => "j"})
+      # => {:"g.h.j"=>"j", :g=>{:h=>{:j=>"j"}}, :"g.h"=>{:j=>"j"}, :"b.c"=>"c", :a=>"a", :b=>{:c=>"c"}}
       #
       def flatten_hash(hash)
         super(unify_hash(hash))
       end
 
+      #
+      # Converts hash containing both flattened and unflattened keys
+      # to only unflattened ones
+      #
+      # unify_hash({:a=>'a', :b=>{:c=>"c"}, :"g.h.j" => "j", :"g.h.k" => "k"})
+      # => {:g=>{:h=>{:j=>"j", :k=>"k"}}, :a=>"a", :b=>{:c=>"c"}}
+      #
+
       def unify_hash(hash)
         flattened_h, regular_h = hash.partition { |k, v| k.to_s.include?(I18n.default_separator) }.map { |a| Hash[a] }
 
-        regular_h.deep_merge(flatten_and_symbolize_keys(flattened_h))
-      end
-
-      #
-      # Expand keys chained by the the given separator through nested Hashes
-      # Similar to I18n::Backend::Helpers#unwind_keys but additinaly deeply symbolize keys
-      # I might be done in 2 steps using I18n::Backend::Helpers#deep_symbolize_keys,
-      # but additional *each* loop makes it much slower
-      #
-      def flatten_and_symbolize_keys(hash, separator = ".")
-        result = {}
-        hash.each do |key, value|
-          keys = key.to_s.split(separator)
-          curr = result
-          curr = curr[((k = keys.shift).to_sym rescue k)] ||= {} while keys.size > 1
-          curr[((k = keys.shift).to_sym rescue k)] = value
-        end
-        result
+        regular_h.deep_merge(deep_symbolize_keys(unwind_keys(flattened_h)))
       end
 
     end
